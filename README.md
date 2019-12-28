9a9445acecce0854d5ae009f95e6195632e41e29
Maciej Olko <maciej.olko@yougov.com>
2019-12-28 20:07:08 +0100

    Add `help translate` sentence with link and resource to README

--- README.md
+++ README.md
@@ -3,7 +3,14 @@ Polskie tłumaczenie dokumentacji Pythona
 ![9.31% language switchera](https://img.shields.io/badge/language_switcher-9.31%25-0.svg)
 ![postęp tłumaczenia całości dokumentacji](https://img.shields.io/badge/dynamic/json.svg?label=całość&query=$.pl&url=http://gce.zhsj.me/python/newest)
 
+[Pomóż tłumaczyć](https://www.transifex.com/python-doc/python-newest/)
+dokumentację Pythona na język polski.
+
+Przydatne materiały:
+* [polskie tłumaczenie dokumentacji Pythona 2.3](https://pl.python.org/docs/).
+
 Znalazłeś błąd lub masz sugestię?
+---------------------------------
 * [Dodaj zgłoszenie](https://github.com/m-aciek/python-docs-pl/issues) w tym projekcie
 * lub sam [nanieś poprawkę](https://www.transifex.com/python-doc/python-newest/)
   w projekcie *Python document translation* na platformie Transifex.
--- manage_translations.py
+++ manage_translations.py
@@ -117,7 +117,14 @@ def recreate_readme():
 ![{language_switcher_status:.2f}% language switchera](https://img.shields.io/badge/language_switcher-{language_switcher_status:.2f}%25-0.svg)
 ![postęp tłumaczenia całości dokumentacji](https://img.shields.io/badge/dynamic/json.svg?label=całość&query=$.{LANGUAGE}&url=http://gce.zhsj.me/python/newest)
 
+[Pomóż tłumaczyć](https://www.transifex.com/python-doc/python-newest/)
+dokumentację Pythona na język polski.
+
+Przydatne materiały:
+* [polskie tłumaczenie dokumentacji Pythona 2.3](https://pl.python.org/docs/).
+
 Znalazłeś błąd lub masz sugestię?
+---------------------------------
 * [Dodaj zgłoszenie](https://github.com/m-aciek/python-docs-pl/issues) w tym projekcie
 * lub sam [nanieś poprawkę](https://www.transifex.com/python-doc/python-newest/)
   w projekcie *Python document translation* na platformie Transifex.
