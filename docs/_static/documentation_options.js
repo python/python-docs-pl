45074baa4ef44e92f8d1f16133e1b239668ed963
GitHub Action's commit-build job <maciej.olko@gmail.com>
2020-02-26 00:18:44 +0000

    Update translation build

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.8.2rc2',
+    VERSION: '3.8.2',
     LANGUAGE: 'pl',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
