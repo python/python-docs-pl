470eb5fd388ff657a811bb30b0d1ea5302bcffdb
Maciej Olko <maciej.olko@yougov.com>
2021-07-27 01:31:59 +0200

    Simiplify README, add image with progress, add script to generate it

---
+++ update_switcher_chart.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+from re import search
+
+from git import Repo, GitCommandError
+from matplotlib import pyplot
+from matplotlib.ticker import PercentFormatter
+
+repo = Repo('.')
+progress, dates = [], []
+for commit in repo.iter_commits():
+    try:
+        readme_content = repo.git.show('{}:{}'.format(commit.hexsha, 'README.md'))
+    except GitCommandError:
+        continue
+    found = search(r'!\[(\d\d.\d\d)% przełącznika języków]', readme_content)
+    if not found:
+        found = search(r'!\[(\d+.\d\d)% language switchera]', readme_content)
+    if not found:
+        found = search(r'!\[(\d+.\d\d)% do language switchera]', readme_content)
+    if not found:
+        print(readme_content)
+        continue
+    number = float(found.group(1))
+    progress.append(number)
+    dates.append(datetime.fromtimestamp(commit.committed_date))
+
+pyplot.plot_date(dates, progress)
+pyplot.ylim(ymin=0)
+pyplot.grid()
+pyplot.gcf().autofmt_xdate()
+pyplot.gca().yaxis.set_major_formatter(PercentFormatter())
+pyplot.savefig("language-switcher-progress.svg")
