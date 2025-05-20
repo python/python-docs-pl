11c1a0da55ba92a60d756d45be4c8de5b1ab1d94
Stan Ulbrych <stanulbrych@gmail.com>
2025-05-20 18:39:13 +0100

    Add script

--- manage_translation.py
+++ manage_translation.py
@@ -11,6 +11,7 @@
 #          files.
 # * recreate_tx_config: recreate configuration for all resources.
 # * warn_about_files_to_delete: lists files that are not available upstream
+# * generate_commit_msg: generates commit message with co-authors
 
 from argparse import ArgumentParser
 import os
@@ -19,7 +20,7 @@ from dataclasses import dataclass
 from difflib import SequenceMatcher
 from logging import info
 from pathlib import Path
-from subprocess import call
+from subprocess import call, run, CalledProcessError
 import sys
 from tempfile import TemporaryDirectory
 from typing import Self, Generator, Iterable
@@ -29,6 +30,8 @@ from polib import pofile
 from transifex.api import transifex_api
 
 LANGUAGE = 'pl'
+PROJECT_SLUG = 'python-newest'
+VERSION = '3.14'
 
 
 def fetch():
@@ -47,10 +50,6 @@ def fetch():
 def _call(command: str):
     if (return_code := call(command, shell=True)) != 0:
         exit(return_code)
-
-
-PROJECT_SLUG = 'python-newest'
-VERSION = '3.14'
 
 
 def recreate_tx_config():
@@ -187,8 +186,39 @@ def language_switcher(entry: ResourceLanguageStatistics) -> bool:
     return any(entry.name.startswith(prefix) for prefix in language_switcher_resources_prefixes)
 
 
+def generate_commit_msg():
+    """Generate a commit message
+    Parses staged files and generates a commit message with Last-Translator's as
+    co-authors.
+    """
+    translators: set[str] = set()
+
+    result = run(["git", "diff", "--cached", "--name-only", "--diff-filter=ACM"], capture_output=True, text=True, check=True)
+    staged = [filename for filename in result.stdout.splitlines() if filename.endswith(".po")]
+
+    for file in staged:
+        staged_file = run(["git", "show", f":{file}"], capture_output=True, text=True, check=True).stdout
+        try:
+            old_file = run(["git", "show", f"HEAD:{file}"], capture_output=True, text=True, check=True).stdout
+        except CalledProcessError:
+            old_file = ""
+
+        new_po = pofile(staged_file)
+        old_po = pofile(old_file) if old_file else POFile()
+        old_entries = {entry.msgid: entry.msgstr for entry in old_po}
+
+        for entry in new_po:
+            if entry.msgstr and (entry.msgid not in old_entries or old_entries[entry.msgid] != entry.msgstr):
+                translator = new_po.metadata.get("Last-Translator")
+                translator = translator.split(",")[0].strip()
+                if translator:
+                    translators.add(f'Co-Authored-By: {translator}')
+                break
+
+    print('Update translation from Transifex\n' + "\n".join(translators))
+
 if __name__ == "__main__":
-    RUNNABLE_SCRIPTS = ('fetch', 'recreate_tx_config', 'warn_about_files_to_delete')
+    RUNNABLE_SCRIPTS = ('fetch', 'recreate_tx_config', 'warn_about_files_to_delete', 'generate_commit_msg')
 
     parser = ArgumentParser()
     parser.add_argument('cmd', choices=RUNNABLE_SCRIPTS)
