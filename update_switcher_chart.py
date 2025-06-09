5ae3158a255f969bb2d610f6a1bcc814e9b70522
Stan Ulbrych <stanulbrych@gmail.com>
2025-06-09 20:19:59 +0100

    Add pre-commit

--- update_switcher_chart.py
+++ update_switcher_chart.py
@@ -7,18 +7,18 @@ from git import Repo, GitCommandError
 from matplotlib import pyplot
 from matplotlib.ticker import PercentFormatter
 
-repo = Repo('.')
+repo = Repo(".")
 progress, dates = [], []
 for commit in repo.iter_commits():
     try:
-        readme_content = repo.git.show('{}:{}'.format(commit.hexsha, 'README.md'))
+        readme_content = repo.git.show("{}:{}".format(commit.hexsha, "README.md"))
     except GitCommandError:
         continue
-    found = search(r'!\[(\d\d.\d\d)% przełącznika języków]', readme_content)
+    found = search(r"!\[(\d\d.\d\d)% przełącznika języków]", readme_content)
     if not found:
-        found = search(r'!\[(\d+.\d\d)% language switchera]', readme_content)
+        found = search(r"!\[(\d+.\d\d)% language switchera]", readme_content)
     if not found:
-        found = search(r'!\[(\d+.\d\d)% do language switchera]', readme_content)
+        found = search(r"!\[(\d+.\d\d)% do language switchera]", readme_content)
     if not found:
         print(readme_content)
         continue
@@ -26,7 +26,7 @@ for commit in repo.iter_commits():
     progress.append(number)
     dates.append(datetime.fromtimestamp(commit.authored_date))
 
-pyplot.plot_date(dates, progress, linestyle='-', marker='')
+pyplot.plot_date(dates, progress, linestyle="-", marker="")
 pyplot.ylim(ymin=0)
 pyplot.grid()
 pyplot.gcf().autofmt_xdate()
