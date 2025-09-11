eff4ae74633df0aa911c21eb88e091a7c9dcbfd2
Maciej Olko <maciej.olko@gmail.com>
2025-09-12 00:07:48 +0200

    [3.13] Fetch 0 percent resources
    To clear out incorrect translations.

--- manage_translation.py
+++ manage_translation.py
@@ -40,7 +40,7 @@ def fetch():
         sys.stderr.write("The Transifex client app is required.\n")
         exit(code)
     lang = LANGUAGE
-    _call(f'tx pull -l {lang} --minimum-perc=1 --force --skip')
+    _call(f'tx pull -l {lang} --force --skip')
     for file in Path().rglob('*.po'):
         _call(f'msgcat --no-location -o {file} {file}')
 
